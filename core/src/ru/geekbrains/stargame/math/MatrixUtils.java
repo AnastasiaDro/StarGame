e99120a94324211f6a44e180d2dcd2c8cda91d50
AnastasiaDro <adrogunova@gmail.com>
2018-10-26 14:01:54 +0300

    Добавлен класс MatrixUtils

---
+++ core/src/ru/geekbrains/stargame/math/MatrixUtils.java
@@ -0,0 +1,43 @@
+package ru.geekbrains.stargame.math;
+
+
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+
+/**
+ * Утилита для работы с матрицами
+ */
+public class MatrixUtils {
+
+    private MatrixUtils() {
+    }
+
+    /**
+     * Расчёт матрицы перехода 4x4
+     * @param mat итоговая матрица преобразований
+     * @param src исходный квадрат
+     * @param dst итоговый квадрат
+     */
+
+
+    // src - прямоуг, с которого хотим проэцироваться, и dst - в который хотим проецироваться
+    // т.е. в качестве прямоуг-в будут выступать наши экраны
+    //рассчитываем матрицу преобразований, учитывая соотношения сторон экрана
+    public static void calcTransitionMatrix(Matrix4 mat, Rect src, Rect dst) {
+        float scaleX = dst.getWidth() / src.getWidth();
+        float scaleY = dst.getHeight() / src.getHeight();
+        mat.idt().translate(dst.pos.x, dst.pos.y, 0f).scale(scaleX, scaleY, 1f).translate(-src.pos.x, -src.pos.y, 0f);
+    }
+
+    /**
+     * Расчёт матрицы перехода 3x3
+     * @param mat итоговая матрица преобразований
+     * @param src исходный квадрат
+     * @param dst итоговый квадрат
+     */
+    public static void calcTransitionMatrix(Matrix3 mat, Rect src, Rect dst) {
+        float scaleX = dst.getWidth() / src.getWidth();
+        float scaleY = dst.getHeight() / src.getHeight();
+        mat.idt().translate(dst.pos.x, dst.pos.y).scale(scaleX, scaleY).translate(-src.pos.x, -src.pos.y);
+    }
+}
